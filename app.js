a338a6ec9b73b813505f697c41b6f4e2fd90a2c5
Sarah Teaw <sarahlst@stanford.edu>
2025-05-05 16:40:56 -0700

    Create separate rooms in query string and sort messages by time

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const supabase = window.supabase.createClient(
   'https://bvcbuwfhagsewfnpkmtg.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2Y2J1d2ZoYWdzZXdmbnBrbXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMTU5ODgsImV4cCI6MjA2MTc5MTk4OH0.GEmQ6QjKff1AB57ja4vs1X9V67SKTr5Cc4UkYZt5sgA'
 );
+const urlParams = new URLSearchParams(window.location.search);
+const room = urlParams.get('room') || 'default';
+document.getElementById('room-label').textContent = `Room: ${room}`;
 
 // Load existing messages
 async function loadMessages() {
   const { data } = await supabase
     .from('messages')
     .select('*')
-    .order('id', { ascending: true });
-
+    .eq('room', room)
+    .order('created_at', { ascending: true });
   data.forEach(addMessageToDOM);
 }
 
@@ -20,7 +23,7 @@ const channel = supabase.channel('public:messages');
 channel
   .on(
     'postgres_changes',
-    { event: 'INSERT', schema: 'public', table: 'messages' },
+    { event: 'INSERT', schema: 'public', table: 'messages', filter: `room=eq.${room}`},
     payload => {
       addMessageToDOM(payload.new);
     }
@@ -39,7 +42,7 @@ function addMessageToDOM(msg) {
 async function sendMessage() {
   const name = document.getElementById('name').value;
   const content = document.getElementById('message').value;
-  await supabase.from('messages').insert([{ user_name: name, content }]);
+  await supabase.from('messages').insert([{ user_name: name, content, room }]);
   document.getElementById('message').value = ''; // Clear input after sending
 }
 
