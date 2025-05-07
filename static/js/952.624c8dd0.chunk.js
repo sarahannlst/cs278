f8b8f393c2b916738dd44e1bf1fd2fd3f948c9f0
Sarah Teaw <sarahlst@stanford.edu>
2025-05-07 09:40:45 -0700

    Updates

---
+++ static/js/952.624c8dd0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpwa_chat_ts=self.webpackChunkpwa_chat_ts||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.624c8dd0.chunk.js.map
